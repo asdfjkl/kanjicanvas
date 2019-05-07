3e4185295193a0dfa00f399ca4615f127e745727
asdfjkl <397515+asdfjkl@users.noreply.github.com>
2019-05-07 23:20:39 +0200

    initial commit

---
+++ read_xml.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET  
+tree = ET.parse('4e3c.xml')  
+root = tree.getroot()
+
+# all item attributes
+print('\nAll attributes:')  
+for elem in root:  
+    print(elem.text.encode("utf-8"))
+    #for subelem in elem:
+        #print(subelem)
+        #print(subelem.attrib)
