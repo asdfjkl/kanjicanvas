34208951f3503c669cb0098ec8e19015c19d54d4
Ubugeeei <ubuge1122@gmail.com>
2023-10-15 04:43:52 +0900

    other: #28 🔹 update examples

--- examples/_basic/src/main.ts
+++ examples/_basic/src/main.ts
@@ -1,14 +1,18 @@
 import "./style.css";
 
 import { KANJI_DATA_SET } from "tegaki/dataset";
+import { recognize } from "tegaki/backend";
 import { createTegaki } from "tegaki/frontend";
 
 const tegaki = createTegaki(document);
-tegaki.init("tegaki-sample", KANJI_DATA_SET);
+tegaki.init("tegaki-sample");
 
 const recognizeBtn = document.getElementById("recognize-btn")!;
 recognizeBtn.addEventListener("click", () => {
-  tegaki.recognize();
+  const strokes = tegaki.getStrokes();
+  const candidate = recognize(strokes, KANJI_DATA_SET);
+  const candidateContainer = document.getElementById("candidate-container")!;
+  candidateContainer.textContent = candidate.join(", ");
 });
 
 const undoBtn = document.getElementById("undo-btn")!;
