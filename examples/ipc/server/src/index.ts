89ce1ddc709f44cb58583f6947a1d8dd9f1550f9
Ubugeeei <ubuge1122@gmail.com>
2023-10-15 13:16:35 +0900

    feat: #30 💪 encode payload

--- examples/ipc/server/src/index.ts
+++ examples/ipc/server/src/index.ts
@@ -4,14 +4,28 @@ import { cors } from "hono/cors";
 
 import { KANJI_DATA_SET } from "@tegaki/dataset";
 import { recognize } from "@tegaki/backend";
+import { DecodeError, decodeStroke } from "@tegaki/utils";
 
 const app = new Hono();
 
 app.use("/recognize", cors());
+app.use("/recognize-encoded", cors());
 
 app.post("/recognize", async c => {
   const req = await c.req.json();
+  console.log(`size: ${JSON.stringify(req.strokes).length} bytes`);
   const strokes = req.strokes;
+  const candidate = recognize(strokes, KANJI_DATA_SET);
+  return c.json(candidate);
+});
+
+app.post("/recognize-encoded", async c => {
+  const req = await c.req.json();
+  console.log(`size: ${req.strokes.length} bytes`);
+  const strokes = decodeStroke(req.strokes);
+  if (strokes instanceof DecodeError) {
+    return c.json({ error: strokes.message });
+  }
   const candidate = recognize(strokes, KANJI_DATA_SET);
   return c.json(candidate);
 });
